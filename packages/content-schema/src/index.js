4dc6eb04f616fde56411e6a3f289e3b17ee156bb
림림 <hyerim23@ewhain.net>
2026-08-16 20:27:46 +0900

    chore: 잔여 폴더 정리

--- packages/content-schema/src/index.js
+++
@@ -1,3 +0,0 @@
-export { PUZZLE_COUNT_LIMITS, suggestPuzzleCount } from "./puzzle-count.js";
-export { validatePuzzlePack } from "./validate-puzzle-pack.js";
-export { buildGenerationPrompt } from "./build-generation-prompt.js";
